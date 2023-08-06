cd9dc4de1f8f88ee995a886679b3a9adb21dfce4
Dragon336699 <anhruoia1a1@gmail.com>
2023-08-06 21:09:27 +0700

    6/8/2023

--- src/components/Slide/SlideSection.js
+++ src/components/Slide/SlideSection.js
@@ -1,40 +1,55 @@
 import { Col, Container, Row } from 'react-bootstrap'
 import styles from './slidesection.module.css'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { logRoles } from '@testing-library/react'
 function Slide() {
-    const [hide, setHide] = useState(true)
     const [elements, setElements] = useState([])
+    const [isAnimating, setIsAnimating] = useState(false)
     useEffect(() => {
         const slideWrap = document.querySelectorAll(`.${styles.slide_wrapper}`)
         setElements(slideWrap)
     }, [])
 
+    if (elements.length) {
+        elements.forEach(item => {
+            item.classList.remove(`${styles.slide_active}`)
+        })
+        elements[0].classList.add(`${styles.slide_active}`)
+        elements[0].style.transition =''
+    }
+    
     const handleSlideRight = () => {
-
-        setElements((prevElements) => {
+        setIsAnimating(true)
+        setElements(prevElements => {
             const [first, ...rest] = prevElements;
+            // rest[0].style.transition = ''
+            // first.style.transition = ''
+            first.style.left = '-100%'
+            setTimeout(() => {
+                first.style.transition = 'none'
+                first.style.left = '100%'
+                setIsAnimating(false)
+            }, 800)
+            // rest[1].style.transition = ''
             return [...rest, first]
         })
-        elements.forEach((item) => {
-            item.hidden = true;
-        })
-        elements[0].hidden = false
     }
 
     const handleSlideLeft = () => {
-
+        setIsAnimating(true)
         setElements(prevElements => {
-            
-            const elementsCopy = [...prevElements]; 
+            const elementsCopy = [...prevElements];
             const last = elementsCopy.pop();
+            elementsCopy[0].style.left = '100%'
+            elementsCopy[elementsCopy.length - 1].style.transition = 'none'
+            elementsCopy[elementsCopy.length - 1].style.left = '-100%'
+            setTimeout(() => {
+                // elementsCopy[elementsCopy.length - 1].style.transition = ''
+                setIsAnimating(false)
+            }, 800)
             return [last, ...elementsCopy];
         })
-        console.log(elements)
 
-        elements.forEach((item) => {
-            item.hidden = true;
-        })
-        elements[0].hidden = false
     }
 
 
@@ -48,16 +63,7 @@ function Slide() {
                     <span className='mt-3'>02</span>
                 </div>
                 <Col className={`${styles.slide_detailBox} `} md='10' lg='10'>
-                    <div className={`${styles.slide_wrapper}`}>
-                        <h2>Welcome</h2>
-                        <h1 className={`${styles.slide_detailBox_name}`}>Flowers shop</h1>
-                        <p className={`${styles.slide_detailBox_descrip} `}>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum has been the industry's standard dummy text ever since
-                        </p>
-                        <button className={`${styles.slide_button_buy} btn btn-dark`}>Buy now</button>
-                    </div>
-                    <div hidden className={`${styles.slide_wrapper} `}>
+                    <div className={`${styles.slide_wrapper} ${styles.slide_active}`}>
                         <h2>Welcome 1</h2>
                         <h1 className={`${styles.slide_detailBox_name}`}>Flowers shop</h1>
                         <p className={`${styles.slide_detailBox_descrip} `}>
@@ -66,7 +72,8 @@ function Slide() {
                         </p>
                         <button className={`${styles.slide_button_buy} btn btn-dark`}>Buy now</button>
                     </div>
-                    <div hidden className={`${styles.slide_wrapper} `}>
+
+                    <div className={`${styles.slide_wrapper}`}>
                         <h2>Welcome 2</h2>
                         <h1 className={`${styles.slide_detailBox_name}`}>Flowers shop</h1>
                         <p className={`${styles.slide_detailBox_descrip} `}>
@@ -75,11 +82,34 @@ function Slide() {
                         </p>
                         <button className={`${styles.slide_button_buy} btn btn-dark`}>Buy now</button>
                     </div>
+                    <div className={`${styles.slide_wrapper}`}>
+                        <h2>Welcome 3</h2>
+                        <h1 className={`${styles.slide_detailBox_name}`}>Flowers shop</h1>
+                        <p className={`${styles.slide_detailBox_descrip} `}>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since
+                        </p>
+                        <button className={`${styles.slide_button_buy} btn btn-dark`}>Buy now</button>
+                    </div>
                     <div className={`${styles.slideButton__wrap}`}>
-                        <button onClick={handleSlideLeft} className={`${styles.slideButton__control}`}>
+                        <button onClick={(e) => {
+                            if (isAnimating) {
+                                e.preventDefault()
+                            }
+                            else {
+                                return handleSlideLeft()
+                            }
+                        }} className={`${styles.slideButton__control}`}>
                             <i className="fa-solid fa-arrow-left"></i>
                         </button>
-                        <button onClick={handleSlideRight} className={`${styles.slideButton__control}`}>
+                        <button onClick={(e) => {
+                            if (isAnimating) {
+                                e.preventDefault()
+                            }
+                            else {
+                                return handleSlideRight()
+                            }
+                        }} className={`${styles.slideButton__control}`}>
                             <i className="fa-solid fa-arrow-right"></i>
                         </button>
                     </div>
